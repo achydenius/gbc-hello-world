910e8c9aea5e95109e71ee2a934ca50899c4691a
Arto Chydenius <arto.chydenius@reaktor.com>
2021-02-24 18:13:17 +0200

    Load and display a bitmap image

--- Makefile
+++ Makefile
@@ -6,8 +6,11 @@ fix: build
 build: main.o
 	rgblink -o main.gb main.o
 
-main.o: main.asm
+main.o: main.asm r.2bpp
 	rgbasm -o main.o main.asm
 
+r.2bpp: r.png
+	rgbgfx -v -o r.2bpp r.png
+
 clean:
-	rm -f main.gb *.o
+	rm -f main.gb *.o *.2bpp
--- main.asm
+++ main.asm
@@ -1,5 +1,10 @@
 INCLUDE "hardware.inc"
 
+MAP_WIDTH   EQU 32
+GFX_WIDTH   EQU 10
+GFX_HEIGHT  EQU 12
+GFX_OFFSET  EQU 5 + (3 * MAP_WIDTH)
+
 SECTION "VBlank", ROM0[$40]
     jp VBlankHandler
 
@@ -9,10 +14,27 @@ SECTION "Header", ROM0[$100]
 SECTION "Code", ROM0[$150]
 Start:
     di                  ; Disable interrupts
+
+    call WaitVBlank     ; Turn off LCD
+    ld hl, rLCDC
+    res 7, [hl]
+
     ld a, IEF_VBLANK    ; Set v-blank interrupt handler
     ld [rIE], a
+
+    ld hl, Tiles        ; Copy tiles to VRAM
+    ld bc, _VRAM
+    ld a, GFX_WIDTH * GFX_HEIGHT
+    call DMACopy
+
+    call SetupTileMap   ; Set up the image centered
+
     ld a, $0            ; Reset offset counter
     ld [Offset], a
+
+    ld hl, rLCDC        ; Turn on LCD
+    set 7, [hl]
+
     ei                  ; Enable interrupts
 
 Loop:
@@ -40,6 +62,51 @@ VBlankHandler:
 
     reti
 
+SetupTileMap:
+    ld a, $0                    ; a = tile index
+    ld b, GFX_HEIGHT            ; b = row
+    ld hl, _SCRN0 + GFX_OFFSET
+.row:
+    ld c, GFX_WIDTH             ; c = column
+.col:
+    ld [hl+], a
+    inc a
+    dec c
+    jp nz, .col
+
+    ld de, MAP_WIDTH - GFX_WIDTH
+    add hl, de
+    dec b
+    jp nz, .row
+
+    ret
+
+WaitVBlank:
+    ld a, [rLY]
+    cp a, $90
+    jp c, WaitVBlank
+    ret
+
+; hl = Source address
+; bc = Target address
+; a = Number of 16 byte blocks to copy
+DMACopy:
+    ld d, a         ; Save a
+
+    ld a, h         ; Set source
+    ld [rHDMA1], a
+    ld a, l
+    ld [rHDMA2], a
+
+    ld a, b         ; Set target
+    ld [rHDMA3], a
+    ld a, c
+    ld [rHDMA4], a
+
+    ld a, d         ; Start general purpose DMA transfer
+    ld [rHDMA5], a
+    ret
+
 Gradient:
 COL SET 0
     REPT 31
@@ -51,6 +118,10 @@ COL SET COL + 1
 COL SET COL - 1
     ENDR
     DW $0
+
+SECTION "Tiles", ROM0, ALIGN[4]
+Tiles:
+    INCBIN "r.2bpp"
 
 SECTION "Variables", WRAM0[$C000]
 Offset: DS 1
