f2503a1e221bdf9a5a46566ceace022baa6ff3b2
Arto Chydenius <arto.chydenius@reaktor.com>
2021-03-07 18:53:47 +0200

    Clean up code a bit

--- main.asm
+++ main.asm
@@ -1,11 +1,11 @@
 INCLUDE "hardware.inc"
 
-MAP_WIDTH       EQU 32
-GFX_WIDTH       EQU 10
-GFX_HEIGHT      EQU 12
-GFX_OFFSET      EQU 5 + (3 * MAP_WIDTH)
+LCDCF_ON_BIT    EQU 7
+LOGO_WIDTH      EQU 10
+LOGO_HEIGHT     EQU 12
+LOGO_OFFSET_X   EQU 5
+LOGO_OFFSET_Y   EQU 3
 GRADIENT_STEPS  EQU 64
-SINE_STEPS      EQU 256
 
 SECTION "VBlankVector", ROM0[$40]
     jp VBlankHandler
@@ -18,70 +18,73 @@ SECTION "Header", ROM0[$100]
 
 SECTION "Code", ROM0[$150]
 Start:
-    di                          ; Disable interrupts
+    di                              ; Disable interrupts
 
-    call WaitVBlank             ; Turn off LCD
+    call WaitVBlank                 ; Turn off LCD
     ld hl, rLCDC
-    res 7, [hl]
+    res LCDCF_ON_BIT, [hl]
 
-    ld a, IEF_VBLANK | IEF_LCDC ; Enable v-blank and status interrupts
+    ld a, IEF_VBLANK | IEF_LCDC     ; Enable vblank and status interrupts
     ld [rIE], a
-    ld a, STATF_MODE00          ; Set h-blank to cause a status interrupt
+    ld a, STATF_MODE00              ; Set hblank to cause a status interrupt
     ld [rSTAT], a
 
-    ld hl, Tiles                ; Copy tiles to VRAM
+    ld hl, Tiles                    ; Copy tiles to VRAM
     ld bc, _VRAM
-    ld a, GFX_WIDTH * GFX_HEIGHT
+    ld a, LOGO_WIDTH * LOGO_HEIGHT
     call DMACopy
 
-    call SetupTileMap           ; Set up the image centered
+    ld hl, TileMap                  ; Copy tilemap to VRAM
+    ld bc, _SCRN0
+    ld a, 32
+    call DMACopy
 
-    ld a, $0                    ; Reset variables
+    xor a, a                        ; Reset variables
     ld hl, Variables
-    REPT 6
+    ld b, 6
+.resetVariable:
     ld [hl+], a
-    ENDR
+    dec b
+    jr nz, .resetVariable
 
-    ld hl, rLCDC                ; Turn on LCD
-    set 7, [hl]
+    ld hl, rLCDC                    ; Turn on LCD
+    set LCDCF_ON_BIT, [hl]
 
-    ld a, KEY1F_PREPARE         ; Set CPU double speed mode
+    ld a, KEY1F_PREPARE             ; Set CPU double speed mode
     ld [rKEY1], a
     stop
 
-    ei                          ; Enable interrupts
+    ei                              ; Enable interrupts
 
 Loop:
-    ld a, ~P1F_5                ; Read button states
+    ld a, ~P1F_5                    ; Read button states
     ld [rP1], a
     ld a, [rP1]
-    cpl                         ; A set bit 0 indicates pressed A button
-    and a, $1
+    cpl                             ; Complement bits so that set bit indicates a pressed button
+    and a, $1                       ; Ignore others than A button
 
-    ld b, a                     ; Compare current and previous button states
-    ld a, [ButtonState]
-    cp a, b
+    ld hl, ButtonState              ; Compare previous and current button state and update the variable
+    cp a, [hl]
+    ld [hl], a
 
-    ld a, b                     ; Update button state variable
-    ld [ButtonState], a
+    jr z, Loop                      ; If current and previous state were equal, do nothing
+    cp a, $0                        ; If current state is button press, do nothing
+    jr z, Loop
 
-    jr z, Loop                  ; Continue if current and previous states are equal
-    cp a, $0                    ; Continue if current state is button press
-    jp z, Loop
-
-    ld a, [EffectOn]            ; Toggle effect state variable
+    ld a, [EffectOn]                ; Toggle effect state variable
     xor a, $1
     ld [EffectOn], a
-    ld a, $0                    ; Reset horizontal scroll register
+
+    xor a, a                        ; Reset horizontal scroll register
     ld [rSCX], a
     jr Loop
 
 HBlankHandler:
     push af
-    push bc
+    push hl
 
-    ld hl, Gradient             ; Calculate address to gradient color
-    ld a, [HBColorOffset]       ; Address is offset times six (two-byte colors in groups of three)
+    ld hl, Gradient                 ; Calculate address to gradient color
+    ld a, [HBColorOffset]           ; Address is offset times six (two-byte colors in groups of three)
     rla
     ld b, $0
     ld c, a
@@ -89,23 +92,23 @@ HBlankHandler:
     add hl, bc
     add hl, bc
 
-    ld a, BCPSF_AUTOINC         ; Set first palette index with auto increment
+    ld a, BCPSF_AUTOINC             ; Set first palette index with auto increment
     ld [rBCPS], a
     REPT 6
-    ld a, [hl+]                 ; Update palette byte by byte
+    ld a, [hl+]                     ; Update palette byte by byte
     ld [rBCPD], a
     ENDR
 
-    ld a, [HBColorOffset]       ; Increment color hblank offset
+    ld a, [HBColorOffset]           ; Increment color hblank offset
     inc a
-    and a, (GRADIENT_STEPS- 1)
+    and a, (GRADIENT_STEPS - 1)
     ld [HBColorOffset], a
 
-    ld a, [EffectOn]            ; Apply sine effect only if effect variable is toggled
+    ld a, [EffectOn]                ; Apply sine effect only if effect variable is toggled
     cp a, $0
     jr z, .return
 
-    ld a, [HBSineOffset]        ; Calculate address to sine table
+    ld a, [HBSineOffset]            ; Calculate address to sine table
     ld b, $0
     ld c, a
     ld hl, Sine
@@ -114,81 +117,80 @@ HBlankHandler:
     ld [rSCX], a
 
 .return:
-    ld a, [HBSineOffset]        ; Increment sine hblank offset
-    inc a
-    and a, (SINE_STEPS-1)
-    ld [HBSineOffset], a
+    ld hl, HBSineOffset             ; Increment sine hblank offset
+    inc [hl]
 
-    pop bc
+    pop hl
     pop af
     reti
 
 VBlankHandler:
     push af
-    push bc
+    push hl
 
-    ld a, [VBColorOffset]       ; Increment color vblank offset and
-    dec a                       ; start color hblank offset from vblank offset
+    ld a, [VBColorOffset]           ; Increment color vblank offset and
+    dec a                           ; start color hblank offset from vblank offset
     ld [VBColorOffset], a
     ld [HBColorOffset], a
 
-    ld a, [VBSineOffset]        ; Increment sine vblank offset and
-    inc a                       ; start sine hblank offset from vblank offset
+    ld a, [VBSineOffset]            ; Increment sine vblank offset and
+    inc a                           ; start sine hblank offset from vblank offset
     ld [VBSineOffset], a
     ld [HBSineOffset], a
 
-    pop bc
+    pop hl
     pop af
     reti
-
-SetupTileMap:
-    ld a, $0                    ; a = tile index
-    ld b, GFX_HEIGHT            ; b = row
-    ld hl, _SCRN0 + GFX_OFFSET
-.row:
-    ld c, GFX_WIDTH             ; c = column
-.col:
-    ld [hl+], a
-    inc a
-    dec c
-    jp nz, .col
-
-    ld de, MAP_WIDTH - GFX_WIDTH
-    add hl, de
-    dec b
-    jp nz, .row
-
-    ret
 
 WaitVBlank:
     ld a, [rLY]
     cp a, $90
-    jp c, WaitVBlank
+    jr nz, WaitVBlank
     ret
 
 ; hl = Source address
 ; bc = Target address
 ; a = Number of 16 byte blocks to copy
 DMACopy:
-    ld d, a                     ; Save a
+    ld d, a                         ; Save a
 
-    ld a, h                     ; Set source
+    ld a, h                         ; Set source
     ld [rHDMA1], a
     ld a, l
     ld [rHDMA2], a
 
-    ld a, b                     ; Set target
+    ld a, b                         ; Set target
     ld [rHDMA3], a
     ld a, c
     ld [rHDMA4], a
 
-    ld a, d                     ; Start general purpose DMA transfer
+    ld a, d                         ; Start general purpose DMA transfer
     ld [rHDMA5], a
     ret
 
 SECTION "Data", ROM0, ALIGN[4]
 Tiles:
     INCBIN "monoglyph.2bpp"
+
+TileMap:
+OFF = 0
+Y = 0
+    REPT 18
+X = 0
+        REPT 32
+            IF Y >= LOGO_OFFSET_Y && \
+               Y < LOGO_HEIGHT + LOGO_OFFSET_Y && \
+               X >= LOGO_OFFSET_X && \
+               X < LOGO_WIDTH + LOGO_OFFSET_X
+                DB OFF
+OFF = OFF + 1
+            ELSE
+                DB 0
+            ENDC
+X = X + 1
+        ENDR
+Y = Y + 1
+    ENDR
 
 Gradient:
     INCLUDE "gradient.inc"
